8d78f6d7803f3f23c66cc686cb8c88e3cac8d297
gbchap <gb.freitaschap@gmail.com>
2026-05-13 17:20:20 -0300

    fix(sidebar): arrumando repositório, consertando problemas

---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+const buttonToggle = document.querySelector('.toggle-sidebar');
+buttonToggle.addEventListener('click', () => {
+   document.querySelector('.sidebar').classList.toggle('closed-sidebar');
+})
