cdafdfa145eb878a7ff3e268a0c31309f72049dc
Heitor Tetzner Pereira <heitortetzner2015@gmail.com>
2026-06-16 16:03:48 -0300

    fix: apos cadastro, move para login

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -11,30 +11,30 @@ class LoginController
     public function efetuaLogin()
     {
         if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    }
-        $email =$_POST['email'];
+            session_start();
+        }
+        $email = $_POST['email'];
         $senha = $_POST['senha'];
 
-        $user = App::get('database') -> verificaLogin(
+        $user = App::get('database')->verificaLogin(
             $email,
             $senha
         );
-        if($user != false)
-        {
-            $_SESSION['id']= $user->id;
-            $_SESSION['nome']= $user->nome;
+        if ($user != false) {
+            $_SESSION['id'] = $user->id;
+            $_SESSION['nome'] = $user->nome;
             $_SESSION['foto'] = $user->foto;
             header('Location: /lista-de-posts');
             exit();
-        } else{
+        } else {
 
 
             $_SESSION['mensagem_erro'] = 'Email ou senha incorretos';
             header('Location: /login');
         }
     }
-    public function exibirLogin(){
+    public function exibirLogin()
+    {
         return view('site/login');
     }
     public function Logout()
@@ -68,11 +68,11 @@ class LoginController
 
         $caminhoFoto = 'public/assets/icon-user.png';
         if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
-        $temporario = $_FILES['imagem']['tmp_name'];
-        $extensao = pathinfo($_FILES['imagem']['name'], PATHINFO_EXTENSION);
-        $nomeUnico = sha1(uniqid($_FILES['imagem']['name'], true)) . '.' . $extensao;
-        $caminhoFoto = 'public/assets/' . $nomeUnico;
-        move_uploaded_file($temporario, $caminhoFoto);
+            $temporario = $_FILES['imagem']['tmp_name'];
+            $extensao = pathinfo($_FILES['imagem']['name'], PATHINFO_EXTENSION);
+            $nomeUnico = sha1(uniqid($_FILES['imagem']['name'], true)) . '.' . $extensao;
+            $caminhoFoto = 'public/assets/' . $nomeUnico;
+            move_uploaded_file($temporario, $caminhoFoto);
         }
 
         $parameters = [
@@ -86,7 +86,7 @@ class LoginController
 
         App::get('database')->insert('usuarios', $parameters);
 
-        header('Location: /lista-de-usuarios');
+        header('Location: /');
         exit();
     }
 }
