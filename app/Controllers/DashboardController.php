72bfd26f5b7ecd5d612e574cca7465dd9d7bfabe
Heitor Tetzner Pereira <heitortetzner2015@gmail.com>
2026-06-17 17:23:09 -0300

    feat: inicia integracao de usuario e quantidades

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -10,6 +10,46 @@ class DashboardController
 
     public function index()
     {
-        return view('admin/dashboard');
+        $database = App::get('database');
+
+        $usuarios = $database->selectAll('usuarios');
+        $usuario = array_filter($usuarios, function ($u) {
+            return (int) $u->id === (int) $_SESSION['id'];
+        });
+        $usuario = reset($usuario);
+
+        $postagens = $database->selectAll('posts');
+
+        $trendingPost = null;
+        $interacoes = $database->selectAll('interacoes');
+        $posts_x_likes = [];
+        foreach ($interacoes as $interacao) {
+            $post = array_filter($postagens, function ($p) use ($interacao) {
+                return (int) $p->id === (int) $interacao->id_post;
+            });
+            $post = reset($post);
+            if (!$post) {
+                continue;
+            }
+
+            $posts_x_likes[$post->id] = ($posts_x_likes[$post->id] ?? 0) + $interacao->likes;
+
+        }
+        if (!empty($posts_x_likes)) {
+            $trendingPostId = array_keys($posts_x_likes, max($posts_x_likes));
+            $trendingPostId = reset($trendingPostId);
+
+            $trendingPost = array_filter($postagens, function ($p) use ($trendingPostId) {
+                return (int) $p->id === (int) $trendingPostId;
+            });
+            $trendingPost = reset($trendingPost);
+        }
+
+        return view('admin/dashboard', [
+            'usuarios' => $usuarios,
+            'usuario' => $usuario,
+            'postagens' => $postagens,
+            'trendingPost' => $trendingPost,
+        ]);
     }
 }
--- app/views/admin/dashboard.view.php
+++ app/views/admin/dashboard.view.php
@@ -7,12 +7,12 @@
     <link rel="stylesheet" href="/public/css/dashboard.css" />
     <script src="https://kit.fontawesome.com/654def639f.js" crossorigin="anonymous"></script>
     <title>Dashboard</title>
-    <?php 
-        $curtidasMes = [1, 3, 5, 7, 6, 5, 7, 8, 9, 10, 13, 14];
+    <?php
+    $curtidasMes = [1, 3, 5, 7, 6, 5, 7, 8, 9, 10, 13, 14];
     ?>
 </head>
 
-<body> 
+<body>
     <div class="espaço-sidebar"></div>
     <main class="dashboard-main">
         <div class="textomain-dashboard">
@@ -26,24 +26,26 @@
                         <p class="titulo-grafico">Curtidas por Mês</p>
                     </div>
                     <div class="grafico-principal">
-                        <?php include('grafico-dash.php');?>
+                        <?php include('grafico-dash.php'); ?>
                     </div>
                 </div>
                 <div class="espaco-numeros-dash">
                     <div class="espaco-user-dash">
                         <i class="fa-solid fa-users i-uspost-dash" style="color: #05ac4b"></i>
-                        <p class="dash-texts">XXX Usuários</p>
+                        <p class="dash-texts"><?= count($usuarios) ?> Usuários</p>
                     </div>
                     <div class="espaco-postagens-dash">
                         <i class="fa-solid fa-note-sticky i-uspost-dash" style="color: #05ac4b"></i>
-                        <p class="dash-texts">XXX Postagens</p>
+                        <p class="dash-texts">
+                            <?= count($postagens) ?> Postagens
+                        </p>
                     </div>
                 </div>
             </div>
             <div class="espaço-analise">
                 <div class="mini-sidecontrol">
                     <div class="side-title-dash">
-                        <h1 class="hello-user-dash">Olá, Usuário.</h1>
+                        <h1 class="hello-user-dash">Olá, <?= $usuario->nome ?>.</h1>
                     </div>
                     <button class="botao-home-dash">
                         <i class="fa-solid fa-house" style="color: white"></i> Home
@@ -55,6 +57,7 @@
                 </div>
                 <div class="trending-posts-dash">
                     <h1 class="trending-dash-text">Trending</h1>
+
                 </div>
             </div>
         </div>
